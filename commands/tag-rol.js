b0415ba52c91c2d55c4664ea2ba84a2a97ad39d9
ardacdx <116028863+ardacdx@users.noreply.github.com>
2022-12-20 22:23:00 +0300

    Create tag-rol.js

---
+++ commands/tag-rol.js
@@ -0,0 +1,16 @@
+client.on("userUpdate", async (oldUser, newUser) => {
+  if (oldUser.username !== newUser.username) {
+    let tag = "!"; //tagınız
+    let sunucu = ""; //sunucu ID
+    let kanal = "" //log kanal id
+    let rol = ""; //tag alınca verilcek rol id
+    if (newUser.username.includes(tag) && !client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.cache.has(rol)) {
+      client.channels.cache.get(kanal).send(`${newUser} **\`${tag}\`** tagını aldığı için <@&${rol}> rolünü kazandı!`)
+      client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.add(rol)
+    } if (!newUser.username.includes(tag) && client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.cache.has(rol)) {
+      client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.remove(rol)
+      client.channels.cache.get(kanal).send(`${newUser} **\`${tag}\`** tagını çıkardığı için <@&${rol}> rolünü kaybetti!`)
+    }
+
+  }
+})
